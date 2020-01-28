db3873c11cd74e3f2cb72d635c4cb2e29647a38b
shareitDOIK <doik.jang@schoolsharing.co.kr>
2020-01-28 20:15:09 +0900

    쉘1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
   public static void main(String[] args) {
-    System.out.println("쉘 테스트");
+    System.out.println("쉘1");
     SpringApplication.run(DemoApplication.class, args);
   }
 
