b5a914db12765bbcf64b333c4815a61b6d776857
shareitDOIK <doik.jang@schoolsharing.co.kr>
2020-01-29 18:42:22 +0900

    깃 푸시

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
   public static void main(String[] args) {
-    System.out.println("쉘5");
+    System.out.println("git conflict test");
     SpringApplication.run(DemoApplication.class, args);
   }
 
